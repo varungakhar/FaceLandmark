9a0891b15f04766990c65e4313a9dc8c4db7dba1
market07 <mj@market07.com>
2018-10-18 21:23:42 +0530

    FaceLanmark

---
+++ Vision/VisionDetection/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  VisionDetection
+//
+//  Created by Varun on 09/02/2018.
+//  Copyright © 2018 Varun. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
